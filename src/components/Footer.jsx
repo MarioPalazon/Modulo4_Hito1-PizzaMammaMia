878f7626aa22c91455c16ec5d4f59e6b68d84580
Mario Palazon <mario.palazon@gmail.com>
2024-08-02 22:14:04 -0400

    ajustes a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-dark text-white text-center my-5 p-4'>
-      <div>© 2021 - Pizzería MammaMia! - Todos los derechos reservados</div>
+    <footer className='bg-dark text-white text-center p-4'>
+      <div>© 2024 - Pizzería MammaMia! - Todos los derechos reservados</div>
     </footer>
   )
 }
